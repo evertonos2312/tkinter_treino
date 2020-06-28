8f6672044f9abf725bcfcbfba013647d8f43f1e6
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-06-28 20:59:25 -0300

    Configurado botão Adcionar, comunicação com banco de dados via SQlite.

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
 from tkinter import *
 from tkinter import ttk
+import sqlite3
 
 root = Tk()
 
+class Funcs():
+    def limpa_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.telefone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
 
-class Application():
+    def login_db(self):
+        self.conn = sqlite3.connect('database.db')
+        self.cursor = self.conn.cursor(); print('Conectando ao banco de dados')
+
+    def logoff_db(self):
+        self.conn.close(); print('Desconectando do banco de dados')
+
+    def montaTabelas(self):
+        self.login_db(); print('Conectando ao banco de dados')
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS clientes(
+            cod INTEGER PRIMARY KEY,
+            nome_cliente CHAR(40) NOT NULL,
+            telefone INTEGER(20),
+            cidade CHAR(40)
+            );
+        """)
+        self.conn.commit(); print("Banco de dados criado")
+        self.logoff_db()
+
+    def add_cliente(self):
+        self.codigo = self.codigo_entry.get()
+        self.nome = self.nome_entry.get()
+        self.telefone = self.telefone_entry.get()
+        self.cidade = self.cidade_entry.get()
+        self.login_db()
+
+        self.cursor.execute("""INSERT INTO clientes(nome_cliente, telefone, cidade)
+        VALUES (?, ?, ?)""", (self.nome, self.telefone, self.cidade))
+        self.conn.commit()
+        self.logoff_db()
+        self.select_lista()
+        self.limpa_tela()
+
+    def select_lista(self):
+        self.listaCli.delete(*self.listaCli.get_children())
+        self.login_db()
+        lista = self.cursor.execute(""" SELECT cod, nome_cliente, telefone, cidade FROM clientes
+        ORDER BY nome_cliente ASC; """)
+        for i in lista:
+            self.listaCli.insert("", END, values=i)
+        self.logoff_db()
+
+
+class Application(Funcs):
     def __init__(self):
         self.root = root
         self.tela()
         self.frames()
         self.widgets_frame1()
         self.lista_frame2()
+        self.montaTabelas()
+        self.select_lista()
         root.mainloop()
 
     def tela(self):
@@ -32,7 +85,7 @@ class Application():
 
     def widgets_frame1(self):
         self.bt_limpar = Button(self.frame_1, text='Limpar', bd=2, bg='#107db2', fg='white',
-                                font=('verdana', 8, 'bold'))
+                                font=('verdana', 8, 'bold'), command=self.limpa_tela)
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
 
         self.bt_buscar = Button(self.frame_1, text='Buscar', bd=2, bg='#107db2', fg='white',
@@ -40,7 +93,7 @@ class Application():
         self.bt_buscar.place(relx=0.301, rely=0.1, relwidth=0.1, relheight=0.15)
 
         self.bt_novo = Button(self.frame_1, text='Novo', bd=2, bg='#107db2', fg='white',
-                                font=('verdana', 8, 'bold'))
+                                font=('verdana', 8, 'bold'), command= self.add_cliente)
         self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
 
         self.bt_alterar = Button(self.frame_1, text='Alterar', bd=2, bg='#107db2', fg='white',
@@ -62,42 +115,42 @@ class Application():
         self.lb_nome = Label(self.frame_1, text='Nome', bg='#dfe3ee', fg='#107db2', font=('verdana', 8, 'bold'))
         self.lb_nome.place(relx=0.05, rely=0.30)
 
-        self.codigo_nome = Entry(self.frame_1)
-        self.codigo_nome.place(relx=0.05, rely=0.40, relwidth=0.4)
+        self.nome_entry = Entry(self.frame_1)
+        self.nome_entry.place(relx=0.05, rely=0.40, relwidth=0.4)
 
         #telefone
 
         self.lb_telefone = Label(self.frame_1, text='Telefone', bg='#dfe3ee', fg='#107db2', font=('verdana', 8, 'bold'))
         self.lb_telefone.place(relx=0.05, rely=0.6)
 
-        self.codigo_telefone = Entry(self.frame_1)
-        self.codigo_telefone.place(relx=0.05, rely=0.70, relwidth=0.4)
+        self.telefone_entry = Entry(self.frame_1)
+        self.telefone_entry.place(relx=0.05, rely=0.70, relwidth=0.4)
 
         #cidade
         self.lb_cidade = Label(self.frame_1, text='Cidade', bg='#dfe3ee', fg='#107db2', font=('verdana', 8, 'bold'))
         self.lb_cidade.place(relx=0.5, rely=0.6, relwidth=0.08)
 
-        self.codigo_cidade = Entry(self.frame_1)
-        self.codigo_cidade.place(relx=0.5, rely=0.7, relwidth=0.4)
+        self.cidade_entry = Entry(self.frame_1)
+        self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
 
     def lista_frame2(self):
-        self.lista_clientes = ttk.Treeview(self.frame_2, height=3, column=('col1', 'col2', 'col3', 'col4'))
-        self.lista_clientes.heading('#0', text='')
-        self.lista_clientes.heading('#1', text='Codigo')
-        self.lista_clientes.heading('#2', text='Nome')
-        self.lista_clientes.heading('#3', text='Telefone')
-        self.lista_clientes.heading('#4', text='Cidade')
+        self.listaCli = ttk.Treeview(self.frame_2, height=3, column=('col1', 'col2', 'col3', 'col4'))
+        self.listaCli.heading('#0', text='')
+        self.listaCli.heading('#1', text='Codigo')
+        self.listaCli.heading('#2', text='Nome')
+        self.listaCli.heading('#3', text='Telefone')
+        self.listaCli.heading('#4', text='Cidade')
 
-        self.lista_clientes.column('#0', width=1)
-        self.lista_clientes.column('#1', width=50)
-        self.lista_clientes.column('#2', width=200)
-        self.lista_clientes.column('#3', width=125)
-        self.lista_clientes.column('#4', width=125)
+        self.listaCli.column('#0', width=1)
+        self.listaCli.column('#1', width=50)
+        self.listaCli.column('#2', width=200)
+        self.listaCli.column('#3', width=125)
+        self.listaCli.column('#4', width=125)
 
-        self.lista_clientes.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+        self.listaCli.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
 
         self.scrollLista = Scrollbar(self.frame_2, orient='vertical')
-        self.lista_clientes.configure(yscroll=self.scrollLista.set)
+        self.listaCli.configure(yscroll=self.scrollLista.set)
         self.scrollLista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
 
 
